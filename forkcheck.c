4b3a85629a9a3c3b7f3315b409f797c6f61aaaa1
Divya <divya@localhost.localdomain>
2026-08-03 05:18:37 +0200

    Add fork process program

--- forkcheck.c
+++ forkcheck.c
@@ -1,1 +1,37 @@
 //C Program to create new process using Linux API system calls fork() and exit()
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fork failed!\n");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        printf("\n----- Child Process -----\n");
+        printf("Child PID  : %d\n", getpid());
+        printf("Parent PID : %d\n", getppid());
+        sleep(2);
+    }
+    else
+    {
+        printf("\n----- Parent Process -----\n");
+        printf("Parent PID : %d\n", getpid());
+        printf("Child PID  : %d\n", pid);
+
+        wait(NULL);
+
+        printf("Child process completed.\n");
+    }
+
+    return 0;
+}
